f0101b7dfe9727578d65f438bc9dfcf75c1ac1ba
Josh Alling <joshralling@gmail.com>
2019-07-12 23:53:57 -0600

    use lawton typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+import lawton from 'typography-theme-lawton'
 
-Wordpress2016.overrideThemeStyles = () => {
+lawton.overrideThemeStyles = () => {
   return {
     'a.gatsby-resp-image-link': {
       boxShadow: `none`,
@@ -9,9 +9,7 @@ Wordpress2016.overrideThemeStyles = () => {
   }
 }
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(lawton)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
